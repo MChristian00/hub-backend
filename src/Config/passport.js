0d93ba5bd8db4f2a16e3d07954f2f5807b70392e
MChristian00 <manzichristian07@gmail.com>
2020-11-25 14:22:14 +0200

    ft(auth): add google and facebook auth
    get id and keys from  google and fb project
    get and add returned user to req object

--- src/Config/passport.js
+++ src/Config/passport.js
@@ -1,6 +1,15 @@
 const LocalStrategy = require("passport-local").Strategy;
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
 const bcrypt = require("bcryptjs");
 const User = require("../Database/Models/User");
+
+const {
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET,
+  FACEBOOK_CLIENT_ID,
+  FACEBOOK_CLIENT_SECRET,
+} = process.env;
 
 module.exports = (passport) => {
   passport.use(
@@ -16,12 +25,45 @@ module.exports = (passport) => {
       }).select("_id FirstName LastName  Password Email createdAt");
     })
   );
-  passport.serializeUser((user, done) => {
-    done(null, user._id);
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: GOOGLE_CLIENT_ID,
+        clientSecret: GOOGLE_CLIENT_SECRET,
+        callbackURL: "/api/auth/login/google/redirect",
+      },
+      (refreshToken, accessToken, profile, cb) => {
+        console.log("GoogleProfile", profile);
+        cb(null, profile);
+      }
+    )
+  );
+  passport.use(
+    new FacebookStrategy(
+      {
+        clientID: FACEBOOK_CLIENT_ID,
+        clientSecret: FACEBOOK_CLIENT_SECRET,
+        callbackURL: "/api/auth/login/facebook/redirect",
+        profileFields: ["id", "email", "displayName"],
+      },
+      (refreshToken, accessToken, profile, cb) => {
+        console.log("FacebookProfile", profile);
+        cb(null, profile);
+      }
+    )
+  );
+
+  passport.serializeUser((user, cb) => {
+    console.log("serial", user);
+    // cb(null, user._id);
+    cb(null, user);
   });
-  passport.deserializeUser((id, done) => {
-    User.findOne({ _id: id }, (err, user) => {
-      done(err, user);
-    });
+
+  passport.deserializeUser((user, done) => {
+    console.log("deserial", user);
+    done(null, user);
+    // User.findOne({ _id: id }, (err, user) => {
+    //   done(err, user);
+    // });
   });
 };
--- src/Routes/UserRoutes.js
+++ src/Routes/UserRoutes.js
@@ -2,7 +2,8 @@ const express = require("express");
 const UsersControllers = require("../Controllers/UsersControllers");
 const UserValidate = require("../Middleware/Validation/UserValidation");
 const passport = require("passport");
-// const tokengen = require('../Helpers/TokenGen')
+const tokengen = require("../Helpers/Helpers");
+const Helpers = require("../Helpers/Helpers");
 
 const router = express.Router();
 
@@ -16,7 +17,39 @@ router.post(
   }),
   UsersControllers.signUser
 );
+router.get(
+  "/login/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+router.get(
+  "/login/google/redirect",
+  passport.authenticate("google", {
+    failureRedirect: "/",
+    // session: false,
+  }),
+  (req, res, next) => {
+    console.log("req", req.user);
+    Helpers.setSuccess(200, "Google authentication successfull", req.user);
+    res.redirect("/go");
+  }
+);
+router.get(
+  "/login/facebook",
+  passport.authenticate("facebook", { scope: ["email"] })
+);
+router.get(
+  "/login/facebook/redirect",
+  passport.authenticate("facebook", {
+    failureRedirect: "/",
+    // session: false,
+  }),
+  (req, res, next) => {
+    console.log("req", req.user);
+    Helpers.setSuccess(200, "Facebook authentication successfull", req.user);
+    res.redirect("/fb");
+  }
+);
 
-router.get("/logout",UsersControllers.logout);
+router.get("/logout", UsersControllers.logout);
 
 module.exports = router;
